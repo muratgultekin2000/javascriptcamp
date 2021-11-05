bd691cbe90a2de279aeada8052f81ef72570293c
muratgultekin2000 <muratgultekin2000@hotmail.com>
2021-11-05 14:13:04 +0300

    javascript1

---
+++ src/functions.js
@@ -0,0 +1,8 @@
+function addToCart(productName="",quantity) {
+    console.log("sepete eklendi!"+productName+
+     "adet:"+quantity)
+}
+addToCart("ekran")
+addToCart("klavye")
+addToCart("fare")
+addToCart()
